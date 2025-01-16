0354e327bc7fcc844cc4af7c2a8305a43b4710b9
Bohdan Dovhyi <79641937+YOOBAMAWHEREISMYCLOTHES@users.noreply.github.com>
2025-01-16 21:37:08 +0200

    Update Navbar.jsx

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './navbar.css';
+import './Navbar.css';
 import navlogo from '../../assets/nav-logo.svg';
 import navProfile from '../../assets/nav-profile.svg';
 
